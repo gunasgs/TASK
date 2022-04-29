f630255719140b17b314d2d1890868333d812cb9
GUNA SGS <gunasgs357@gmail.com>
2022-04-29 19:40:43 +0530

    me

---
+++ task1.js
@@ -0,0 +1,54 @@
+//TASK 1 //
+/*
+const imageURL1 = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF2CVB7bHtKctAvTRZtqSSjMZkrFCqXOkyEg&usqp=CAU.jpg"]
+const landscape1 = document.createElement("img");
+landscape1.setAttribute("src",imageURL1);
+
+document.body.append(landscape1);
+
+
+const imageURL2 = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgqNkr92xFARHPMkflbLhi2XaojbK6Lp0ZKg&usqp=CAU.jpg"]
+const landscape2 = document.createElement("img");
+landscape2.setAttribute("src",imageURL2);
+document.body.append(landscape2);
+
+const imageURL3 = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREgoDB9QYcZaZFEce9cEy_bVoe1taJd_NJ6A&usqp=CAU.jpg"]
+const landscape3 = document.createElement("img");
+landscape3.setAttribute("src",imageURL3);
+document.body.append(landscape3);
+
+const imageURL4 = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMU70RPBai_zdFW40RhkcXeDm3fSpLhQGvyg&usqp=CAU.jpg"]
+const landscape4 = document.createElement("img");
+landscape4.setAttribute("src",imageURL4);
+document.body.append(landscape4);
+
+const imageURL5 = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQSbbrH79c7042exAltvXptKGF2nEzKIGCDw&usqp=CAU.jpg"]
+const landscape5 = document.createElement("img");
+landscape5.setAttribute("src",imageURL5);
+document.body.append(landscape5);
+*/
+
+const allflags = document.createElement("div";
+
+allflags.innerHTML = `
+<img
+src="https://upload.wikimedia.org/wikipedia/commons/7/7b/India_flag_300.png"
+alt="indian flag"
+class="flag"/>s
+`
+
+
+
+
+
+
+/*
+
+const imageURL = ["https://cdn.britannica.com/97/1597-004-05816F4E/Flag-India.jpg"]
+const landscape = document.createElement("img");
+landscape.setAttribute("src",imageURL);
+landscape.setAttribute("class","image");
+document.body.append(landscape);
+
+
+*/
